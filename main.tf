3326a5543fcbb70ff3cc14a656e8ae52eff8a549
diego-souz <diego.oliveira.am@gmail.com>
2023-06-04 18:42:33 -0400

    new

--- main.tf
+++ main.tf
@@ -17,8 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-024e6efaf93d85776"
   instance_type = "t2.micro"
   key_name = "iac-terraform"
-
+  #user_data = <<-EOF
+  #                #!/bin/bash
+  #                cd /home/ubuntu
+  #                echo "<h1>Feito com Terraform</h1>" > index.html 
+  #                nohup busybox httpd -f -p 8080 &
+  #                EOF
+  #                */
   tags = {
-    Name = "Segunda Instancia"
+    Name = "Teste aws"
   }
 }
